83e4829701f5eebf4df0224c80059428bf549098
kalokamil98 <73557117+kalokamil98@users.noreply.github.com>
2021-02-09 00:24:06 +0100

    v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python kurs-powiadomienia.py
---
+++ kurs-powiadomienia.py
@@ -0,0 +1,44 @@
+import os
+from os import environ
+from bs4 import BeautifulSoup
+import requests
+import time
+from dhooks import Webhook
+hook = Webhook(environ['kluczyk'])
+
+
+
+def get_price(waluta):
+  url = 'https://www.google.com/search?q='+waluta+'+to+pln'
+  HTML = requests.get(url)
+
+
+  soup = BeautifulSoup(HTML.text, 'html.parser')
+  #<div class="BNeawe iBp4i AP7Wnd">
+
+  text = soup.find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
+  return text
+
+
+
+def main():
+  last_price = -1
+  while True:
+    kurs = 'bitcoin'
+    price = get_price(kurs)
+    wzrost = int(price[1])
+    
+    if price != last_price:
+      print(kurs +' price: ',price)
+      
+      last_price = price
+      if wzrost == 7:
+        print("kupuje wariacie")
+        hook.send('WZROST !!!')
+    elif wzrost == 5:
+        print("Cenka spada")
+        hook.send("Spadek !!")
+    
+    time.sleep(3) 
+    
+main()
